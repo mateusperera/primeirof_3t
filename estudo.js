8e45a478de23ed836014b3faa8f2fafe7ec15339
mateusperera <110178817+mateusperera@users.noreply.github.com>
2022-10-07 09:58:23 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -29,3 +29,15 @@ function mostratabuada(){
         document.write("O valor do i " + i + "<br>");
     }
 }
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+for(let i =1; i <=t; i++){
+    r = v  * (1 + (j/100));
+    document.write("Mes " + i + " - valor: " + r + "<br>");
+    v = r;
+}
+    document.write("resultado: " + r);
+}
